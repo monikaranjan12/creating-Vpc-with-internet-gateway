597a61d73d13a3f2843dfbec85c53bb6a1f69da9
monikaranjan12 <monikaranjan8252@gmail.com>
2025-11-03 16:17:39 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,32 @@
+output "vpc_id" {
+
+  description = "The ID of the VPC"
+
+  value       = module.vpc.vpc_id
+
+}
+ 
+output "public_subnets" {
+
+  description = "List of public subnet IDs"
+
+  value       = module.vpc.public_subnets
+
+}
+ 
+output "private_subnets" {
+
+  description = "List of private subnet IDs"
+
+  value       = module.vpc.private_subnets
+
+}
+ 
+output "database_subnets" {
+
+  description = "List of database subnet IDs"
+
+  value       = module.vpc.database_subnets
+
+}
+ 
